77e2987252c72bdef86a9bbb1c95c1ffd7603843
ala decoste <alshareefialaa@gmail.com>
2021-02-19 00:52:34 -0500

    Updated indexedDb name.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/assets/css/styles.css',
     '/assets/js/index.js',
-    '/assets/js/indexedDb.js',
+    '/assets/js/indexedDB.js',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     './manifest.json',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
